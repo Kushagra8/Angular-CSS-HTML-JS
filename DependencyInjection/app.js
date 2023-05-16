d7a1cb4d57345c4ce50b0998e3f62be99115acbd
kushagra <Kushagra8@users.noreply.github.com>
2023-05-16 07:47:15 -0400

    injector for DIController

--- DependencyInjection/app.js
+++ DependencyInjection/app.js
@@ -1,14 +1,26 @@
 (function () {
     'use strict';
 
-    angular.module('DIApp',[])
+    angular.module('DIApp', [])
     .controller('DIController', DIController);
 
-    function DIController ($scope, $filter) {
+    function DIController ($scope, $filter, $injector) {
         $scope.name="Kush";
         $scope.upper=function(){
             var upCase=$filter('uppercase');
             $scope.name=upCase($scope.name);
         };
+
+        // gives array of args for DIController
+        console.log($injector.annotate(DIController));
     }
+
+    function AnnotateMe(name, job, blah){
+        return "Blah!";
+    }
+
+     // prints entire functions
+    console.log(AnnotateMe);
+    console.log(DIController.toString());
+
 }) ();
