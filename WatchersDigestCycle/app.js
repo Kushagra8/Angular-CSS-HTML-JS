057b6ade6e8ecfe1d135c5bf250ad952b9fb7dab
kushagra <Kushagra8@users.noreply.github.com>
2023-05-21 08:03:04 -0400

    finish digest loop

--- WatchersDigestCycle/app.js
+++ WatchersDigestCycle/app.js
@@ -9,6 +9,7 @@
 
     $scope.onceCounter = 0;
     $scope.counter = 0;
+    $scope.name = "Kush";
 
     $scope.showNumberOfWatchers = function () {
       console.log('WatchersCount', $scope.$$watchersCount);
@@ -22,14 +23,19 @@
       $scope.counter++;
     }
 
-    $scope.$watch('onceCounter', function (newValue, oldValue) {
-      console.log("old value: ", oldValue);
-      console.log("new value: ", newValue);
-    });
+    $scope.$watch(function () {
+      console.log("Digest Loop Fired!");
+    })
 
-    $scope.$watch('counter', function (newValue, oldValue) {
-      console.log("coutner old value: ", oldValue);
-      console.log("counter new value: ", newValue);
-    });
+    // manual
+    // $scope.$watch('onceCounter', function (newValue, oldValue) {
+    //   console.log("old value: ", oldValue);
+    //   console.log("new value: ", newValue);
+    // });
+
+    // $scope.$watch('counter', function (newValue, oldValue) {
+    //   console.log("coutner old value: ", oldValue);
+    //   console.log("counter new value: ", newValue);
+    // });
   }
 })();
