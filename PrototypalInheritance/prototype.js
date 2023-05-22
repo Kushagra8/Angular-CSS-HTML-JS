8ad400c36b46beb406aac6a0a7d3e9d5d47694e5
kushagra <Kushagra8@users.noreply.github.com>
2023-05-22 19:47:10 -0400

    working on prototypal

--- PrototypalInheritance/prototype.js
+++ PrototypalInheritance/prototype.js
@@ -18,3 +18,12 @@ console.log("PARENT - parent.obj.objValue: ", parent.obj.objValue);
 console.log("parent: ", parent);
 console.log("child: ", child);
 child.value = "childValue";
+
+child.value = "childValue";
+child.obj.objValue = "childObjValue";
+console.Log("*** CHANGED: child.value = 'childvalue'");
+console.log("**** CHANGED: child.obj.objValue = 'childObjvalue'");
+console.log("CHILD - child. value: ", child.value);
+console.log("CHILD - child.obj.objValue: ", child.obj.objValue);
+console.log("PARENT - parent. value: ", parent.value); console.log("PARENT - parent.obj.objValue: ", parent.obj.objvalue);
+console.log("parent: ", parent); console.log("child: ", child);
